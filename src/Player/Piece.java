26ce1ff729764f2928866a98566062a7a1c2ee03
M-Najib Hebrawi <najiphebrawi@hotmail.com>
2020-11-06 14:29:52 +0100

    piece først commit

--- src/Player/Piece.java
+++ src/Player/Piece.java
@@ -1,4 +1,25 @@
 package Player;
 
 public class Piece {
+    private boolean bilLock=false;
+    private boolean racerbilLock= false;
+    private boolean traktorLock=false;
+    private boolean ufoLock=false;
+
+
+
+    public void pieceselect(){
+        System.out.println("Tryk 1 for at være Bilen " +
+                "\nTryk 2 for at være Racerbilen" +
+                " \nTryk 3 for at være Traktoren " +
+                "\nTryk 4 for at være UFO'en");
+
+
+
+
+
+
+
+
+    }
 }
